20d18f14d15a3ed023129558b0ce48c72a9827d3
Igor <test@mail.ru>
2023-07-02 23:05:59 +0300

    Добавил решение задачи 47 (task47)

--- task47/Program.cs
+++ task47/Program.cs
@@ -4,3 +4,44 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+// ---------------------------- Задание функций ---------------------------------------
+double[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    double[,] tempMatrix = new double[row, col];
+    var rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = Math.Round(rand.Next(leftRange, rightRange + 1) + rand.NextDouble(), 3);
+        }
+    }
+
+    return tempMatrix;
+}
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintMatrix(double[,] matrixFotPrint)
+{
+    for (int i = 0; i < matrixFotPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixFotPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixFotPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// ------------------------------
+
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+double[,] matrix = FillMatrix(rows, cols, 0, 100);
+PrintMatrix(matrix);
+
